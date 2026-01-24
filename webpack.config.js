b05826d1a7aa90956744cc1d9b837673c6604c8b
sergeyk1150 <128826073+sergeyk1150@users.noreply.github.com>
2026-01-24 11:42:10 +0300

    build: установка плагина MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -26,5 +27,14 @@ module.exports = {
         },
       ],
     }),
+    new MiniCssExtractPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
 };
