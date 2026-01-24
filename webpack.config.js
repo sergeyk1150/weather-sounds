8b7935a23282c17509db8af45455449316253166
sergeyk1150 <128826073+sergeyk1150@users.noreply.github.com>
2026-01-24 10:40:10 +0300

    build: установка пакета HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
-import path from "path";
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-export default {
+module.exports = {
   context: path.resolve(__dirname, "src"),
   entry: "./index.js",
   output: {
-    filename: "[name].[contenthacsh].js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
 };
