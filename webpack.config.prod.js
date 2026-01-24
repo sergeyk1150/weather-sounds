6c7d8a4ae6277c96d249290cd627f996c3d85e4c
sergeyk1150 <128826073+sergeyk1150@users.noreply.github.com>
2026-01-24 12:14:30 +0300

    build: установка плагина CssMinimizerPlugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,12 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.config.common");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(commonConfig, {
   mode: "production",
   devtool: "source-map",
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
 });
