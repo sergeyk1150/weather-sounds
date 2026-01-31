598b32250fb7fc59e8b189383e2f1a04bfce28d7
sergeyk1150 <128826073+sergeyk1150@users.noreply.github.com>
2026-01-31 10:49:50 +0300

    ...

--- src/core/Dom.js
+++ src/core/Dom.js
@@ -1,35 +1,4 @@
 export class Dom {
-  static create(tag, options = {}) {
-    const el = document.createElement(tag);
-
-    if (options.className) {
-      el.className = options.className;
-    }
-
-    if (options.attr) {
-      Object.entries(options.attrs).forEach(([key, value]) => {
-        el.setAttribute(key, value);
-      });
-    }
-    if (options.style) {
-      Object.assign(el.style, options.style);
-    }
-
-    if (options.events) {
-      Object.entries(options.events).forEach(([event, handler]) => {
-        el.addEventListener(event, handler);
-      });
-    }
-
-    if (options.children) {
-      options.children.forEach((child) => {
-        if (child) el.appendChild(child);
-      });
-    }
-
-    return el;
-  }
-
   static mount(parent, child) {
     parent.appendChild(child);
   }
